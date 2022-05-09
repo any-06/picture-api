8a4bb8feaff76fe84b937013740d3b9982675b7f
any-06 <anyou.6.ali@gmail.com>
2022-05-09 16:39:00 +0200

    Add call JokesTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         $this->call(PictureTableSeeder::class);
+        $this->call(JokesTableSeeder::class);
     }
 }
